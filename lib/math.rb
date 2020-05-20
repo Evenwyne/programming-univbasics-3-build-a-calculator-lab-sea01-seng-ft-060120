4e183588c9580c5896f38f10e1b8155c9be67397
EVEN RICHARDSON <dubbwins@gmail.com>
2020-05-20 07:33:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,8 @@
 def addition(num1, num2)
-
-puts "Enter first num: "
-num1 = gets.chomp
-
-puts "enter second num: "
-num2 = gets.chomp
-
-puts num1.to_f + num2.to_f
-
+  @num1 = gets.chomp
+    @num2 = gets.chomp
+    @num1 + @num2 == @answer
+    puts "The sum is... #{@answer}"
 end
 
 def subtraction(num1, num2)
