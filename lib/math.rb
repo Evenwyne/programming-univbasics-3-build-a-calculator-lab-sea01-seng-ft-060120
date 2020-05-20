0ce15e2e245297064de4d56068ce5a9cc1077fbb
EVEN RICHARDSON <dubbwins@gmail.com>
2020-05-20 07:53:16 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,5 +19,13 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  return  Math.square_root(num).to_i
+  return num if num | 1 == 1
+      bits_shift = (Math.log2(n).ceil)/2 + 1
+      bitn_mask = root = 1 << bits_shift
+      while true
+        root ^= bitn_mask if (root * root) > n
+        bitn_mask >>= 1
+        return root if bitn_mask == 0
+        root |= bitn_mask
+      end
 end
